e1f80fd787f4492a7e3b5ece4ce90228c1879fe1
louisfd <louisfd94@gmail.com>
2025-12-15 11:43:38 -0500

    cubek-test-utils in test only

--- crates/cubek-attention/tests/attention/reference.rs
+++ crates/cubek-attention/tests/attention/reference.rs
@@ -4,7 +4,7 @@ use core::f32;
 
 use cubecl::{TestRuntime, client::ComputeClient, std::tensor::TensorHandle};
 
-use cubek_std::test_utils::{
+use cubek_test_utils::{
     HostData, HostDataType, HostDataVec, StrideSpec, assert_equals_approx,
 };
 
