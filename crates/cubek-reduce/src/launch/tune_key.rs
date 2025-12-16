9dc56e6f06f317fca255f88d8eeaade6c1700dc1
nathaniel <nathaniel.simard.42@gmail.com>
2025-12-16 17:23:39 -0500

    Perf: Improve reduce autotune key

--- crates/cubek-reduce/src/launch/tune_key.rs
+++ crates/cubek-reduce/src/launch/tune_key.rs
@@ -7,12 +7,22 @@ pub struct ReduceAutotuneKey {
     elem_input: ElemType,
     elem_output: ElemType,
     elem_acc: ElemType,
-    potential_line_size: u8,
+    /// Wether the axis is contiguous.
     pub axis_is_contiguous: bool,
-    #[autotune(anchor(exp(min = 16, max = 4096)))]
-    reduce_axis_shape: usize,
-    #[autotune(anchor(exp(max = 16384, base = 4)))]
-    reduce_count: usize,
+    /// The length of the vector to reduce.
+    ///
+    /// # Notes
+    ///
+    /// Max is 4^4, so 5 values are possible.
+    #[autotune(anchor(exp(min = 16, max = 1024, base = 4)))]
+    pub vector_size: usize,
+    /// The number of vectors to reduce.
+    ///
+    /// # Notes
+    ///
+    /// Max is 8^5, so 5 values are possible.
+    #[autotune(anchor(exp(max = 32768, base = 8)))]
+    pub vector_count: usize,
 }
 
 impl ReduceAutotuneKey {
@@ -38,29 +48,13 @@ impl ReduceAutotuneKey {
             .filter_map(|(i, shape)| (i != axis).then_some(shape))
             .product();
 
-        let potential_line_size = Self::potential_line_size(elem_input.size(), reduce_axis_shape);
-
         ReduceAutotuneKey::new(
             elem_input,
             elem_output,
             elem_acc,
-            potential_line_size,
             axis_is_contiguous,
             reduce_axis_shape,
             reduce_count,
         )
-    }
-
-    fn potential_line_size(elem_size: usize, mut shape: usize) -> u8 {
-        let mut potential_line_size = 1;
-        let max_bytes_in_line = 64; // 512 bits
-
-        while shape.is_multiple_of(2)
-            && potential_line_size as usize * elem_size < max_bytes_in_line
-        {
-            potential_line_size *= 2;
-            shape /= 2;
-        }
-        potential_line_size
     }
 }
