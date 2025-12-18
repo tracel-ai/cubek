bd88002d9e1b179c7589bb56a2a92a44061e53f4
louisfd <louisfd94@gmail.com>
2025-12-18 09:17:59 -0500

    fix clippy

--- crates/cubek-matmul/src/definition/base.rs
+++ crates/cubek-matmul/src/definition/base.rs
@@ -81,7 +81,7 @@ impl MatmulProblem {
         }
     }
 
-    #[clippy::allow(too_many_arguments)]
+    #[allow(clippy::too_many_arguments)]
     pub fn from_parameters(
         m: usize,
         n: usize,
