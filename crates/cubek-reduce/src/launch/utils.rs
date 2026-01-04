5427cc80492737821c69ccb005e6702618bfe128
nathaniel <nathaniel.simard.42@gmail.com>
2026-01-04 16:00:42 -0500

    Improve perf

--- crates/cubek-reduce/src/launch/utils.rs
+++ crates/cubek-reduce/src/launch/utils.rs
@@ -87,17 +87,36 @@ pub fn generate_line_size<R: Runtime>(
                 .filter_map(|(i, o)| (i == o).then_some(output.shape[i]))
                 .product();
 
-            tensor_line_size_perpendicular(
-                R::supported_line_sizes()
-                    .into_iter()
-                    .filter(|size| {
-                        **size as usize <= max_line_size && max_line_size % **size as usize == 0
-                    })
-                    .map(|l| *l),
-                input.shape,
-                input.strides,
-                axis,
-            ) as u32
+            match client.properties().hardware.num_cpu_cores.is_some() {
+                true => {
+                    // On CPU we benefit from bigger line size, which increases the number of
+                    // consecutive loads from global memory on perpendicular reduce.
+                    let supported_line_sizes = R::supported_line_sizes()
+                        .into_iter()
+                        .filter(|size| {
+                            **size as usize <= max_line_size && max_line_size % **size as usize == 0
+                        })
+                        .map(|l| *l);
+
+                    tensor_line_size_perpendicular(
+                        supported_line_sizes,
+                        input.shape,
+                        input.strides,
+                        axis,
+                    ) as u32
+                }
+                false => {
+                    let supported_line_sizes =
+                        client.io_optimized_line_sizes_unchecked(dtype.size());
+
+                    tensor_line_size_perpendicular(
+                        supported_line_sizes,
+                        input.shape,
+                        input.strides,
+                        axis,
+                    ) as u32
+                }
+            }
         }
     };
 
