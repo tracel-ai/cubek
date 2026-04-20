0db64a10aa14546a5b8482a6ead44ad79f82b3cc
louisfd <louisfd94@gmail.com>
2026-04-20 15:25:13 -0400

    rm dead and add scope constraint

--- crates/cubek-matmul/src/components/stage/matmul/plane_partitioned/matmul.rs
+++ crates/cubek-matmul/src/components/stage/matmul/plane_partitioned/matmul.rs
@@ -7,7 +7,8 @@ use crate::{
 use cubecl::{prelude::*, std::tensor::layout::Coords2d};
 
 use crate::components::{
-    stage::matmul::partition::SharedPartitionMatmulConfig, tile_matmul::TileConfig,
+    stage::matmul::partition::SharedPartitionMatmulConfig,
+    tile_matmul::{Plane, TileConfig},
 };
 
 #[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
@@ -33,6 +34,7 @@ pub type PlaneMatmul<
             <MP::Rhs as MatrixTypes>::RegisterSize,
             <MP::Acc as MatrixTypes>::Register,
             <MP::Acc as MatrixTypes>::RegisterSize,
+            Scope = Plane,
         >,
     StageLhs,
     StageRhs,
