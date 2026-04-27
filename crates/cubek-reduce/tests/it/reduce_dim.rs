8cbb4cf9852c26bc440c9d828417ab828c4bcc15
louisfd <louisfd94@gmail.com>
2026-04-27 11:09:09 -0400

    ignore argtopk

--- crates/cubek-reduce/tests/it/reduce_dim.rs
+++ crates/cubek-reduce/tests/it/reduce_dim.rs
@@ -1,5 +1,9 @@
 use cubecl::zspace::{Shape, Strides};
-use cubek_reduce::{ReduceStrategy, launch::VectorizationStrategy, routines::{BlueprintStrategy, plane::PlaneStrategy}};
+use cubek_reduce::{
+    ReduceStrategy,
+    launch::VectorizationStrategy,
+    routines::{BlueprintStrategy, plane::PlaneStrategy},
+};
 
 use crate::it::test_case::TestCase;
 
@@ -14,6 +18,7 @@ pub fn test_argmin() {
 }
 
 #[test]
+#[ignore = "ArgTopK not yet supported"]
 pub fn test_argtopk() {
     test_case().test_argtopk(3);
 }
@@ -31,15 +36,15 @@ pub fn test_topk_5() {
 #[test]
 pub fn test_plane_reduction_simple() {
     let strategy = ReduceStrategy {
-        vectorization: VectorizationStrategy{ parallel_output_vectorization: true },
-        routine: RoutineStrategy::Plane(BlueprintStrategy::Inferred(PlaneStrategy{ independent:  true })),
+        vectorization: VectorizationStrategy {
+            parallel_output_vectorization: true,
+        },
+        routine: RoutineStrategy::Plane(BlueprintStrategy::Inferred(PlaneStrategy {
+            independent: true,
+        })),
     };
-    let test_case = TestCase::new::<TestDType>(
-        Shape::new([16]),
-        Strides::new(&[1]),
-        Some(0),
-        strategy,
-    );
+    let test_case =
+        TestCase::new::<TestDType>(Shape::new([16]), Strides::new(&[1]), Some(0), strategy);
 
     test_case.test_topk(3);
 }
