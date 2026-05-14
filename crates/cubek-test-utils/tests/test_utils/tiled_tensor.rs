3e13c5f075243f61af5dc14c19e194f7a4fe930f
Sublime Tshimpangila <tshimangasublime@gmail.com>
2026-05-14 11:31:50 -0400

    remove first version of tiled layout

--- crates/cubek-test-utils/tests/test_utils/tiled_tensor.rs
+++ crates/cubek-test-utils/tests/test_utils/tiled_tensor.rs
@@ -11,7 +11,7 @@ use cubecl::{
 use cubek_test_utils::{HostData, HostDataType, StrideSpec, TestInput, assert_equals_approx};
 
 #[test]
-fn read_rowmajor_tensor_as_tiled_layout_v2() {
+fn read_rowmajor_tensor_as_tiled_layout() {
     let client = <TestRuntime as Runtime>::client(&Default::default());
 
     let matrix_len = 4;
@@ -73,54 +73,6 @@ fn read_rowmajor_tensor_as_tiled_layout_v2() {
         .enforce()
 }
 
-#[test]
-fn read_rowmajor_tensor_as_tiled() {
-    let client = <TestRuntime as Runtime>::client(&Default::default());
-
-    let matrix_len = 4;
-    let shape = shape![matrix_len, matrix_len];
-    let input_handle = TestInput::builder(client.clone(), shape.clone())
-        .stride(StrideSpec::RowMajor)
-        .arange()
-        .generate();
-
-    let dtype = f32::as_type_native_unchecked().storage_type();
-    let output_handle = TestInput::builder(client.clone(), shape.clone())
-        .stride(StrideSpec::RowMajor)
-        .zeros()
-        .generate_without_host_data();
-
-    let cube_count = CubeCount::new_single();
-    let cube_dim = CubeDim::new_single();
-    let vector_size = 1;
-    launch_read_rowmajor_tensor_as_tiled::launch::<TestRuntime>(
-        &client,
-        cube_count,
-        cube_dim,
-        input_handle.binding().into_tensor_arg(),
-        output_handle.clone().binding().into_tensor_arg(),
-        dtype,
-        vector_size,
-        matrix_len,
-    );
-
-    let output = HostData::from_tensor_handle(&client, output_handle, HostDataType::F32);
-
-    #[rustfmt::skip]
-    let expected_values = [
-        0.000,  1.000,  4.000,  5.000,
-        2.000,  3.000,  6.000,  7.000,
-        8.000,  9.000, 12.000, 13.000,
-        10.00, 11.000, 14.000, 15.000,
-    ].to_vec();
-    let (_, expected_values) = TestInput::builder(client, shape)
-        .custom(expected_values)
-        .generate_with_f32_host_data();
-    assert_equals_approx(&output, &expected_values, 1e-6)
-        .as_test_outcome()
-        .enforce()
-}
-
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct TilerBlueprint {
     pub start_axis: u8,
@@ -141,67 +93,6 @@ impl MetadataBlueprint {
             strides,
             tiler,
         }
-    }
-}
-
-#[derive(CubeType, Clone, Copy)]
-pub struct TiledLayout {
-    width: usize,
-    height: usize,
-    tile_w: usize,
-    tile_h: usize,
-}
-
-#[cube]
-impl TiledLayout {
-    pub fn new(width: usize, height: usize, tile_w: usize, tile_h: usize) -> TiledLayout {
-        TiledLayout {
-            width,
-            height,
-            tile_w,
-            tile_h,
-        }
-    }
-}
-
-#[cube]
-impl Layout for TiledLayout {
-    type Coordinates = (usize, usize);
-
-    type SourceCoordinates = Coords1d;
-
-    fn to_source_pos(&self, pos: Self::Coordinates) -> Self::SourceCoordinates {
-        let row = pos.0;
-        let col = pos.1;
-
-        let tile_row = row / self.tile_h;
-        let tile_col = col / self.tile_w;
-
-        let local_row = row % self.tile_h;
-        let local_col = col % self.tile_w;
-
-        let tiles_per_row = self.width / self.tile_w;
-
-        let tile_index = tile_row * tiles_per_row + tile_col;
-        let tile_area = self.tile_h * self.tile_w;
-
-        let block_offset = tile_index * tile_area;
-        let local_offset = local_row * self.tile_w + local_col;
-
-        block_offset + local_offset
-    }
-
-    fn to_source_pos_checked(&self, pos: Self::Coordinates) -> (Self::SourceCoordinates, bool) {
-        let is_valid = pos.0 < self.height && pos.1 < self.width;
-        (self.to_source_pos(pos), is_valid)
-    }
-
-    fn shape(&self) -> Self::Coordinates {
-        (self.width, self.height)
-    }
-
-    fn is_in_bounds(&self, _pos: Self::Coordinates) -> bool {
-        true.runtime()
     }
 }
 
@@ -248,30 +139,6 @@ impl Layout for RowMajorLayout {
 }
 
 #[cube(launch)]
-fn launch_read_rowmajor_tensor_as_tiled<N: Numeric, S: Size>(
-    input: &Tensor<Vector<N, S>>,
-    output: &mut Tensor<Vector<N, S>>,
-    #[define(N)] _dtype: StorageType,
-    #[define(S)] vector_size: usize,
-    #[comptime] matrix_len: usize,
-) {
-    let input_view = input.view(TiledLayout::new(
-        matrix_len,
-        matrix_len,
-        matrix_len / 2,
-        matrix_len / 2,
-    ));
-    let output_view = output.view_mut(RowMajorLayout::new(matrix_len, matrix_len, vector_size));
-
-    for i in 0..matrix_len {
-        for j in 0..matrix_len {
-            let value = input_view.read((i, j));
-            output_view.write((i, j), value);
-        }
-    }
-}
-
-#[cube(launch)]
 fn launch_read_tensor_as_tiled<N: Numeric, S: Size>(
     input: &Tensor<Vector<N, S>>,
     output: &mut Tensor<Vector<N, S>>,
@@ -301,7 +168,7 @@ fn launch_read_tensor_as_tiled<N: Numeric, S: Size>(
         tiles.push(comptime!(tiler.tile_size[i]) as usize);
     }
 
-    let tiled_layout = TiledLayoutV2::new(shape, strides, tiler.start_axis as usize, tiles);
+    let tiled_layout = TiledLayout::new(shape, strides, tiler.start_axis as usize, tiles);
 
     let row_major = RowMajorLayout::new(4, 4, vector_size);
 
@@ -322,7 +189,7 @@ fn launch_read_tensor_as_tiled<N: Numeric, S: Size>(
 }
 
 #[derive(CubeType, Clone)]
-pub struct TiledLayoutV2 {
+pub struct TiledLayout {
     shape: Sequence<usize>,
     strides: Sequence<usize>,
     #[cube(comptime)]
@@ -331,14 +198,14 @@ pub struct TiledLayoutV2 {
 }
 
 #[cube]
-impl TiledLayoutV2 {
+impl TiledLayout {
     pub fn new(
         shape: Sequence<usize>,
         strides: Sequence<usize>,
         #[comptime] start_axis: usize,
         tiles: Sequence<usize>,
-    ) -> TiledLayoutV2 {
-        TiledLayoutV2 {
+    ) -> TiledLayout {
+        TiledLayout {
             shape,
             strides,
             start_axis,
@@ -348,7 +215,7 @@ impl TiledLayoutV2 {
 }
 
 #[cube]
-impl Layout for TiledLayoutV2 {
+impl Layout for TiledLayout {
     type Coordinates = Sequence<usize>;
 
     type SourceCoordinates = Coords1d;
